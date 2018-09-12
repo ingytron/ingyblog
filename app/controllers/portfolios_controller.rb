e15b1b5748e450c1a5beb95a954dadf198faa88d
Ingrid Molloy <ingrid.molloy@shopify.com>
2018-09-11 22:14:51 -0700

    dry-ed up portfolio params

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -15,9 +15,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
-                                                  technologies_attributes: [:name]))
-
+    @portfolio_item = Portfolio.new(portfolio_params)
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Portfolio item has been added.' }
@@ -34,7 +32,7 @@ class PortfoliosController < ApplicationController
   def update
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
       else
         format.html { render :edit}
@@ -48,6 +46,12 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html {redirect_to portfolios_url, notice: 'Portfolio item was responsibly recycled on your behalf.'}
     end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
 
 end
---
+++ building my shopify shop.md
@@ -0,0 +1,37 @@
+Day 1 -- Friday
+
+After taking a look at some stores for sale, I couldn't
+afford any of the ones I wanted, so I decided to take
+matters into my own hands and create my own Shopify
+store.
+
+Created a new store, added Oberlo app to find products,
+stayed up until 2am adding and importing products.
+
+At this point I already had a domain registered that
+I wasn't using which was a pretty good fit for the
+name, but I didn't have any images to use.
+
+Day 2 -- Saturday
+
+After sleeping, logged back into my store only to have
+the sad realization that I'd imported products the
+wrong way and was missing a bunch of variant, spent
+quite a few hours adding more products, looking for
+the things I wanted and re-importing products I'd
+imported the wrong way.
+
+Wanted to add a theme to improve the look of the site
+but also wanted to save money so went for a free
+theme. I added some stock images to the slideshow,
+and some of the default colours. It's still looking
+largely un-customised but it's a start.
+
+Created a FaceBook page for my new venture, added an
+Instagram account and did more product editing.
+
+Day 3 - Sunday
+
+I had to go out for the day but I got some editing
+done via the mobile app. Not much progress today.
+Made some menu changes.
